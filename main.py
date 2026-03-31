cf787036a7c610f794f4af3ad1c4435313edb5ef
Aleksey-laptop-linux <aleksshugol2911@gmail.com>
2026-03-31 12:03:44 +0300

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+# Точка входа в программу.
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from MainWindow import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
